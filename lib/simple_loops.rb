9faa526cb96725ca19650bec5ee8953f3fe3854e
james wu <jwu_nyc@yahoo.com>
2020-06-26 20:07:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,7 +24,7 @@ def return_string_array(array)
   new_array=[]
   counter=0
   while counter<array.length
-  new_array[counter]=array[counter].to_S
+  new_array[counter]=array[counter].to_s
   counter+=1
   end
   new_array
