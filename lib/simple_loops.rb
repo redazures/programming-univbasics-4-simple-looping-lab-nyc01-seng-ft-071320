9f89438bb249cf0c0c681361ae86e2ce946a1cef
james wu <jwu_nyc@yahoo.com>
2020-06-26 20:00:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,7 +13,10 @@ def loop_message_n_times(string,num)
 end
 
 def output_array(array)
+  array.each do {|item| puts item}
 end
 
-def return_string_array
+def return_string_array(array)
+  new_array=[]
+  array.each do{|item|new_array<<item.to_s}
 end
