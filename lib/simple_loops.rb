068dcc2545a6475cb5d1126df971eea86bc80946
james wu <jwu_nyc@yahoo.com>
2020-06-26 20:01:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,6 +16,10 @@ def output_array(array)
   array.each do {|item| puts item}
 end
 
+def output_array2(array)
+  array.each do {|item| puts item}
+end
+
 def return_string_array(array)
   new_array=[]
   array.each do{|item|new_array<<item.to_s}
